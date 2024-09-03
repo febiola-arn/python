a58c3cacfc6f6c465789187410ffd9bdf6785821
febiola_arn <aruanfebiola13@gmail.com>
2024-09-03 12:42:27 +0700

    perkuliahan 1

---
+++ 1_iintro.py
@@ -0,0 +1,26 @@
+
+# deklarasi variabel
+a = float(10.5)
+b = 5
+c = a + b
+print(a)
+print(b)
+print(c)
+
+print("Hello World")
+
+#operasi (operation)-> tambah(+),kurang(-),bagi(/),kali(*),modulo(%),pangkat(**)
+pertambahan = a+b
+perkalian = a*b
+pembagian = a/b
+pengurangan = a-b
+modulo = a%b
+pangkat = a**b
+
+print(pertambahan)
+print(perkalian)
+print(pembagian)
+print(pengurangan)
+print(modulo)
+print(pangkat)
+
