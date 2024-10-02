d2ab6c32df286ffa91b9d7ac20830c5adb421ec5
febiola_arn <aruanfebiola13@gmail.com>
2024-10-02 13:29:21 +0700

    2_intro.py

---
+++ 2_intro.py
@@ -0,0 +1,34 @@
+# perulangan -> Looping
+
+for baris in range (5) :
+    for kolom in range (5):
+        #jika end nya tidak di tambahkan maka hasil nya akan terurut ke bawah bukan kesamping
+        print("*",end="")
+    print()    
+
+    
+
+#while itu untuk perulangan yang lebih kecil dari 10
+#while iteration -> need condition 
+
+baris2 = 0
+tengah = 5
+while baris2 < 5 :
+    kolom2 = 0 
+    if(baris2 % 2 == 0):
+        while kolom2 < 5:
+            if(kolom2 == int(round(tengah/2))):
+               print("*",end="")
+            else:
+                print(" ",end="")
+            kolom2 += 1
+    else:
+        while kolom2 < 5 :
+            print("*",end="")
+            kolom2 += 1
+    print()
+    baris2 += 1
+
+    
+
+
